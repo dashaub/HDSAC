4d03471b31971eb1b241760b8ec9bf1bd352ace0
David Shaub <dashaub@users.noreply.github.com>
2018-12-12 10:33:55 -0700

    Create davidshaub_code.sh

---
+++ davidshaub_code.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -euo pipefail
+
+echo 'Running simulation'
+Rscript davidshaub_code.R
+
+echo 'Creating davidshaub_artifact.gif'
+convert -loop 0 -resize 800x800 1*.png davidshaub_artifact.gif
